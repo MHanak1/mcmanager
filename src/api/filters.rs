8863832d0256ecd30c35c3a46d55481c477900d7
MHanak1 <michalhanak@duck.com>
2025-07-02 12:29:37 +0200

    fix auth for real this time

--- src/api/filters.rs
+++ src/api/filters.rs
@@ -3,6 +3,7 @@ use crate::database::{Database, DatabaseError};
 use axum::extract::{FromRequest, FromRequestParts, Request};
 use axum::http::request::Parts;
 use futures::{TryFutureExt, TryStreamExt};
+use log::debug;
 use reqwest::StatusCode;
 use sqlx::encode::IsNull::No;
 use crate::api::serve::AppState;
@@ -11,34 +12,33 @@ use crate::api::handlers::handle_database_error;
 
 pub struct BearerToken(pub Uuid);
 
-impl<S: Sync> FromRequestParts<S> for BearerToken {
+impl<S: Sync + std::marker::Send> FromRequestParts<S> for BearerToken {
     type Rejection = StatusCode;
 
     async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection>{
-        let header = parts.headers.get("Authorization");
+        let header = parts.headers.get("Authentication");
         if let Some(header) = header {
             if let Ok(header) = header.to_str() {
                 if header[0..7] == *"Bearer " {
                     if let Ok(token) = Uuid::parse_str(&header[7..]) {
+                        debug!("found session token in header: {}", token);
                         return Ok(BearerToken(token));
                     }
                 }
             }
         }
 
-        let cookies = parts.headers.get_all("Cookie");
-        for cookie in cookies {
-            if let Ok(cokie) = cookie.to_str() {
-                if let Some((name, value)) = cokie.split_once("=") {
-                    if name == "session-token" {
-                        if let Ok(token) = Uuid::parse_str(value) {
-                            return Ok(BearerToken(token));
-                        }
-                    }
+        if let Ok(cookies)  = axum_extra::extract::CookieJar::from_request_parts(parts, state).await {
+            if let Some(cookie) = cookies.get("session-token") {
+                println!("found session token in cookie: {}", cookie.value());
+                if let Ok(token) = Uuid::parse_str(cookie.value()) {
+                    debug!("found session token in cookie: {}", token);
+                    return Ok(BearerToken(token));
                 }
             }
         }
 
+        debug!("auth token not found");
         Err(StatusCode::UNAUTHORIZED)
 
     }
@@ -52,7 +52,16 @@ impl FromRequestParts<AppState> for WithSession {
     async fn from_request_parts(parts: &mut Parts, state: &AppState) -> Result<Self, Self::Rejection> {
         let token = BearerToken::from_request_parts(parts, state).await?;
 
-        Ok(Self(state.get_where("token", token.0, None).await.map_err(handle_database_error)?))
+        match state.get_where::<Session, _>("token", token.0, None).await.map_err(handle_database_error) {
+            Ok(session) => {
+                debug!("found session: {}", session.id);
+                Ok(Self(session))
+            },
+            Err(err) => {
+                debug!("error getting session: {}", err);
+                Err(StatusCode::UNAUTHORIZED)
+            }
+        }
     }
 }
 
@@ -64,6 +73,15 @@ impl FromRequestParts<AppState> for UserAuth {
     async fn from_request_parts(parts: &mut Parts, state: &AppState) -> Result<Self, Self::Rejection> {
         let session = WithSession::from_request_parts(parts, state).await?;
 
-        Ok(Self(state.get_one(session.0.user_id, None).await.map_err(handle_database_error)?))
+        match state.get_one::<User>(session.0.user_id, None).await.map_err(handle_database_error) {
+            Ok(user) => {
+                debug!("found user: {}", user.id);
+                Ok(Self(user))
+            }
+            Err(err) => {
+                debug!("error getting user: {}", err);
+                Err(StatusCode::UNAUTHORIZED)
+            }
+        }
     }
 }
